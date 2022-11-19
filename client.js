47434ae091dd1e478509f1b10a773b5cb792c28e
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-11-19 23:27:34 +0000

    Add JSON sockets

--- client.js
+++ client.js
@@ -1,38 +1,60 @@
-const net = require('net');
+const net = require("net");
+const JsonSocket = require("json-socket");
 
 const client = new net.Socket();
 const port = 6969;
 const host = "127.0.0.1";
 
 client.connect(port, host, () => {
-    console.log("Connected");
+	console.log(`Connection established with server ${host}:${port}`);
 
-    client.on("data", (data) => {
-        console.log(JSON.parse(data));
+	client.on("data", (data) => {
+		console.log(JSON.parse(data));
 
-        client.end();
-    })
+		client.end();
+	})
 });
 
 const putMessage = {
-    t: "P",
-    d: {
-        id: 1,
-        first_name: "Logan",
-        last_name: "Kollaschek",
-        email: "lkollaschek0@dedecms.com",
-        gender: "Male"
-    }
+
+		id: 1,
+		first_name: "Logan",
+		last_name: "Kollaschek",
+		email: "lkollaschek0@dedecms.com",
+		gender: "Male"
 };
 
-const getMessage = {
-    t: "G",
-    d: {
-        key: 1
-    }
+function put(key, value){
+	const message = {
+		type: "PUT",
+		key,
+		value
+	};
+
+	JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
+		if (error) 
+			throw error;
+
+		console.log("PUT RESPONSE: ");
+		console.log(response);
+	});
 };
 
-client.write(JSON.stringify(putMessage));
+function get(key) {
+	const message = {
+		type: "GET",
+		key
+	};
 
-client.write(JSON.stringify(getMessage));
+	JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
+		if (error)
+			throw error;
 
+		console.log("GET RESPONSE: ");
+		console.log(response);
+	});
+}
+
+put("xxx", putMessage);
+
+get("xxx");
