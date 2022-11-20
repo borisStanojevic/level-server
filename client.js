b115ea114d89c199cd4db94e304ae2409dfd9309
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-11-20 21:15:42 +0000

    Update client.js

--- client.js
+++ client.js
@@ -1,58 +1,65 @@
 const net = require("net");
 const JsonSocket = require("json-socket");
+const {
+    nanoid
+} = require("nanoid");
 
 const client = new net.Socket();
 const port = 6969;
 const host = "127.0.0.1";
 
 client.connect(port, host, () => {
-	console.log(`Connection established with server ${host}:${port}`);
+    console.log(`Connection established with server ${host}:${port}`);
 
-	client.on("data", (data) => {
-		console.log(JSON.parse(data));
+    client.on("data", (data) => {
+        console.log(JSON.parse(data));
 
-		client.end();
-	})
+        client.end();
+    })
 });
 
 const putMessage = {
 
-		id: 1,
-		first_name: "Logan",
-		last_name: "Kollaschek",
-		email: "lkollaschek0@dedecms.com",
-		gender: "Male"
+    id: 1,
+    first_name: "Logan",
+    last_name: "Kollaschek",
+    email: "lkollaschek0@dedecms.com",
+    gender: "Male"
 };
 
-function put(key, value){
-	const message = {
-		type: "PUT",
-		key,
-		value
-	};
+function createUUID() {
+    return nanoid();
+}
 
-	JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
-		if (error) 
-			throw error;
+function put(key, value) {
+    const message = {
+        type: "PUT",
+        key,
+        value
+    };
 
-		console.log("PUT RESPONSE: ");
-		console.log(response);
-	});
+    JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
+        if (error)
+            throw error;
+
+        console.log("PUT RESPONSE: ");
+        console.log(response);
+    });
 };
 
 function get(key) {
-	const message = {
-		type: "GET",
-		key
-	};
+    const message = {
+        type: "GET",
+        key
+    };
 
-	JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
-		if (error)
-			throw error;
+    JsonSocket.sendSingleMessageAndReceive(port, host, message, function(error, response) {
+        if (error)
+            throw error;
 
-		console.log("GET RESPONSE: ");
-		console.log(response);
-	});
+        console.log("GET RESPONSE: ");
+        console.log(response);
+    });
 }
 
 put("xxx", putMessage);
